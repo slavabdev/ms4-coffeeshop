affb901b633ff12f9df2d44217388a5d99196797
slavabdev <slav.balykov@gmail.com>
2021-06-16 23:15:17 +0000

    added product_detail template

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
+    path('<slug:product_slug>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
-    '''A view to show an individual product details'''
+    '''A view to show all products'''
     products = Product.objects.all()
     context = {
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_slug):
+    '''A view to show an individual product details'''
+    product = get_object_or_404(Product, slug=product_slug)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
