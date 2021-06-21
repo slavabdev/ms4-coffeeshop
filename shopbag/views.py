78bd356df6d7081dd8cb6581efd52563825060dd
slavabdev <slav.balykov@gmail.com>
2021-06-21 11:27:10 +0000

    udated sizes functionality in shopping cart

--- shopbag/views.py
+++ shopbag/views.py
@@ -5,16 +5,28 @@ def the_bag(request):
     '''Returning the the shopping bag page'''
     return render(request, 'shopbag/shopbag.html')
 
-def add_item_bag (request, item_id):
+
+def add_item_bag(request, item_id):
     ''' add item to the bag '''
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     shopbag = request.session.get('shopbag', {})
 
-    if item_id in list(shopbag.keys()):
-        shopbag[item_id] += quantity
-    else: 
-        shopbag[item_id] = quantity
-        request.session['shopbag'] = shopbag
-        
-        return redirect(redirect_url)
+    if size:
+        if item_id in list(shopbag.keys()):
+            if size in shopbag[item_id]['items_by_size'].keys():
+                shopbag[item_id]['items_by_size'][size] += quantity
+            else:
+                shopbag[item_id]['items_by_size'][size] = quantity
+        else:
+            shopbag[item_id] = {'items_by_size': {size: quantity}}
+    else:
+        if item_id in list(shopbag.keys()):
+            shopbag[item_id] += quantity
+        else:
+            shopbag[item_id] = quantity
+    request.session['shopbag'] = shopbag
+    return redirect(redirect_url)
