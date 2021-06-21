d546fcbb7015d93e02e8e68b49ffaf6b978765c9
slavabdev <slav.balykov@gmail.com>
2021-06-21 00:22:07 +0000

    added add item to bag function

--- shopbag/urls.py
+++ shopbag/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.the_bag, name='the_bag'),
+    path('add/<item_id>/', views.add_item_bag, name='add_item_bag'),
 ]
--- shopbag/views.py
+++ shopbag/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 
 def the_bag(request):
-    '''Returning the index page'''
+    '''Returning the the shopping bag page'''
     return render(request, 'shopbag/shopbag.html')
+
+def add_item_bag (request, item_id):
+    ''' add item to the bag '''
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shopbag = request.session.get('shopbag', {})
+
+    if item_id in list(shopbag.keys()):
+        shopbag[item_id] += quantity
+    else: 
+        shopbag[item_id] = quantity
+        request.session['shopbag'] = shopbag
+        print(request.session['shopbag'])
+        return redirect(redirect_url)
