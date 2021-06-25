842d579f0233889222ae7bfbdfd473093b1417bb
slavabdev <slav.balykov@gmail.com>
2021-06-25 02:34:51 +0000

    filled the checkout success template; added overlay

--- checkout/models.py
+++ checkout/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-from products.models import Product 
+from products.models import Product
 
 
 class Order(models.Model):
