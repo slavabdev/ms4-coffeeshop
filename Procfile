ee573e7606b1e20765c24422dc8b0b181e81cdb3
slavabdev <slav.balykov@gmail.com>
2021-06-29 19:50:55 +0000

    added new settitngs for deployment purp

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn coffeeshop.wsgi:application
--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7eguv40((w5&4+7^-7cd-zzt!h($nx55r0=kgm_-nxyg$_1pm^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-coffee-cup.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'coffeeshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
